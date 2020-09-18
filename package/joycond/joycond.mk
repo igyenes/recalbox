30fa827711a6285929e3e63d11b1dd4ac5a9e607
David Barbion <davidb@230ruedubac.fr>
2020-09-18 22:56:45 +0200

    feat(joycond): add joycond package for Switch joycon support

---
+++ package/joycond/joycond.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# joycond
+#
+################################################################################
+
+JOYCOND_VERSION = 2990fe2b156697e7eca6f32be5a2d22ed26f6386
+JOYCOND_SITE = $(call github,DanielOgorchock,joycond,$(JOYCOND_VERSION))
+JOYCOND_DEPENDENCIES = eudev libevdev
+
+define JOYCOND_INIT_INSTALL
+	$(INSTALL) -D -m 0644 $(JOYCOND_PKGDIR)/71-joycond.rules $(TARGET_DIR)/lib/udev/rules.d/71-joycond.rules
+	$(INSTALL) -D -m 0755 $(JOYCOND_PKGDIR)/joycond-init $(TARGET_DIR)/etc/init.d/S17joycond
+endef
+
+JOYCOND_POST_INSTALL_TARGET_HOOKS += JOYCOND_INIT_INSTALL
+
+$(eval $(cmake-package))
