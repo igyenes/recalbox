bff88e96511716bc30338af8117a5edf33f98d7e
David Barbion <davidb@230ruedubac.fr>
2020-09-20 12:59:05 +0200

    feat(joycond): add missing udev rules, initscript and dependency on hid-nintendo

--- package/joycond/joycond.mk
+++ package/joycond/joycond.mk
@@ -6,7 +6,7 @@
 
 JOYCOND_VERSION = 2990fe2b156697e7eca6f32be5a2d22ed26f6386
 JOYCOND_SITE = $(call github,DanielOgorchock,joycond,$(JOYCOND_VERSION))
-JOYCOND_DEPENDENCIES = eudev libevdev
+JOYCOND_DEPENDENCIES = eudev libevdev hid-nintendo
 
 define JOYCOND_INIT_INSTALL
 	$(INSTALL) -D -m 0644 $(JOYCOND_PKGDIR)/71-joycond.rules $(TARGET_DIR)/lib/udev/rules.d/71-joycond.rules
