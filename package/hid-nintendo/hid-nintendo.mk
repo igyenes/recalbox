144f79e592fc135a9cdec60b9b21c6d1b210dc82
David Barbion <davidb@230ruedubac.fr>
2020-11-20 21:46:03 +0100

    chore(hid-nintendo): bump hid-nintendo package

--- package/hid-nintendo/hid-nintendo.mk
+++ package/hid-nintendo/hid-nintendo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HID_NINTENDO_VERSION = ca684eacf372a21bacffaddf018c2ea2b776a07f
+HID_NINTENDO_VERSION = 5371d385c93e0e6ed34b6a8990d4c960a58bc88d
 HID_NINTENDO_SITE = $(call github,nicman23,dkms-hid-nintendo,$(HID_NINTENDO_VERSION))
 HID_NINTENDO_LICENSE = GPL-2.0
 HID_NINTENDO_LICENSE_FILES = LICENSE
